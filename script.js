92bc8e360687a63bdd92b3542b00998300eb0e3e
rizanopriatmoko <rizanopriatmoko98@gmail.com>
2021-03-25 21:33:17 +0700

    DOM+HTML-Table-Cart

--- script.js
+++ script.js
@@ -1,111 +1,88 @@
+let gameTemp = ''
+let hargaTemp = 0
 
-let gameGta = {
-        name: 'GTA V', //id = gta
-        price: 600000,
-        description: 'An action-adventure game developed by Rockstar North and published by Rockstar Games.',
-    }
-let gameFifa = {
-        name: 'FIFA 21', //id = fifa
-        price: 500000,
-        description: 'A football simulation video game published by Electronic Arts as part of the FIFA series.',
-    }
-let gameFfxv = {
-        name: 'Final Fantasy XV', //id = ffxv
-        price: 600000,
-        description: 'An action role-playing game developed and published by Square Enix. The fifteenth main installment of the Final Fantasy series.',
-    }
-let gameMhw = {
-        name: 'Monster Hunter World', //id = mhw
-        price: 700000,
-        description: 'An action role-playing game developed and published by Capcom and the fifth mainline installment in the Monster Hunter series',
-    }
-let gameCyberpunk ={
-        name: 'Cyberpunk', //id = cyberpunk
-        price: 800000,
-        description: 'A 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe.',
-    }
-let gameRdr2 ={
-        name: 'Red Dead Redemption 2',//id = rdr2
-        price: 600000,
-        description: 'Red Dead Redemption 2 is a 2018 action-adventure game developed and published by Rockstar Games.',
-    }
-let gamePersona = {
-        name: 'Persona 5',//id = persona
-        price: 650000,
-        description: 'Persona 5 is a role-playing video game developed by Atlus. It is the sixth installment in the Persona series, which is part of the larger Megami Tensei franchise.',
-    }
-let gameHarvest = {
-        name: 'Harvest Moon',//id = harvest
-        price: 250000,
-        description: 'Harvest Moon is a farm simulation role-playing game developed by Amccus for the Super Nintendo Entertainment System. ',
-    }
 
-let container = []
-let containerHarga = []
-
-let total = 0
-
-function tampil() {
-    var tabel = document.getElementById("tabel");
-    tabel.innerHTML = "<tr><th>No</th><th>Nama Game</th><th>harga</th><th>Action</th></tr>";
-    for (let i = 0; i < container.length; i++) {
-        var btnCancel = "<button class='btn-Cancel' href='#' onclick='Cancel(" + i + ")'>Cancel</button>";
-        j = i + 1;
-        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + container[i] + "</td><td>" + containerHarga[i] + "</td><td>" + btnCancel + "</tr>";
-        total += containerHarga[i]
-    }
+function insertNewRecord() {
+    var table = document.getElementById("tabel").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = gameTemp;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = hargaTemp;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = `<a onClick="onDelete(this)">Cancel</a>`;
+    totalHarga()
 }
 
 function buyGta() {
-    var input = document.getElementById("gta");
-    container.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
+    var input = "GTA V";
+    var inputHarga = 600000  
+    containerHarga.push(inputHarga)
+    hargaTemp = inputHarga
+    gameTemp = input
+    insertNewRecord();
 }
 function buyFifa() {
-    var input = document.getElementById("fifa");
-    data.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
+    var input = "FIFA 21";
+    var inputHarga = 500000   
+    containerHarga.push(inputHarga)
+    hargaTemp = inputHarga
+    gameTemp = input
+    insertNewRecord();
 }
 function buyFfxv() {
-    var input = document.getElementById("Ffxv");
-    data.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
+    var input = 'Final Fantasy 15'
+    var inputHarga = 600000 
+    containerHarga.push(inputHarga)
+    hargaTemp = inputHarga
+    gameTemp = input
+    insertNewRecord();
 }
 function buyMhw() {
-    var input = document.getElementById("Mhw");
-    data.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
+    var input = 'Monster Hunter World'
+    var inputHarga = 700000   
+    containerHarga.push(inputHarga)
+    hargaTemp = inputHarga
+    gameTemp = input
+    insertNewRecord();
 }
 function buyCyberpunk() {
-    var input = document.getElementById("cyberpunk");
-    data.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
+    var input = 'Cyberpunk'
+    var inputHarga = 800000    
+    containerHarga.push(inputHarga)
+    hargaTemp = inputHarga
+    gameTemp = input
+    insertNewRecord();
 }
 function buyRdr2() {
-    var input = document.getElementById("rdr2");
-    data.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
-}
-function buyPersona() {
-    var input = document.getElementById("Persona");
-    data.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
-}
-function buyHarvest() {
-    var input = 'document.getElementById("Harvest")';
-    data.push(input.name);
-    containerHarga.push(input.price)
-    tampil();
+    var input = 'Red Dead Redemption 2'
+    var inputHarga = 600000   
+    containerHarga.push(inputHarga)
+    hargaTemp = inputHarga
+    gameTemp = input
+    insertNewRecord();
 }
 
-function cancel(id) {
-    data.pop(id);
-    tampil();
+function buyPersona() {
+    var input = 'Persona 5'
+    var inputHarga = 650000   
+    containerHarga.push(inputHarga)
+    hargaTemp = inputHarga
+    gameTemp = input
+    insertNewRecord();
 }
+
+function onDelete(td) {
+    if (confirm('Are you sure to cancel')) {
+        row = td.parentElement.parentElement;
+        containerHarga.splice(row.rowIndex-1, 1)
+        console.log(containerHarga)
+        document.getElementById("tabel").deleteRow(row.rowIndex);
+        totalHarga()
+    }
+}
+let hargaTotal = 0
+function totalHarga(){
+    document.getElementById("total").innerHTML = `Total = ${containerHarga.reduce((a, b) => a + b, 0)}`
+}
+let containerHarga = []
