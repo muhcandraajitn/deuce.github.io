b9077fb83ae6f7b143cf53a3928ab5a534da2308
rizanopriatmoko <rizanopriatmoko98@gmail.com>
2021-03-26 10:10:38 +0700

    scriptCart

--- scriptCart.js
+++ scriptCart.js
@@ -1,39 +1,3 @@
-
-let gameGta = {
-        name: 'GTA V', //id = gta
-        price: 600000,
-        description: 'An action-adventure game developed by Rockstar North and published by Rockstar Games.',
-    }
-let gameFifa = {
-        name: 'FIFA 21', //id = fifa
-        price: 500000,
-        description: 'A football simulation video game published by Electronic Arts as part of the FIFA series.',
-    }
-let gameFfxv = {
-        name: 'Final Fantasy XV', //id = ffxv
-        price: 600000,
-        description: 'An action role-playing game developed and published by Square Enix. The fifteenth main installment of the Final Fantasy series.',
-    }
-let gameMhw = {
-        name: 'Monster Hunter World', //id = mhw
-        price: 700000,
-        description: 'An action role-playing game developed and published by Capcom and the fifth mainline installment in the Monster Hunter series',
-    }
-let gameCyberpunk ={
-        name: 'Cyberpunk', //id = cyberpunk
-        price: 800000,
-        description: 'A 2020 action role-playing video game developed and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe.',
-    }
-let gameRdr2 ={
-        name: 'Red Dead Redemption 2',//id = rdr2
-        price: 600000,
-        description: 'Red Dead Redemption 2 is a 2018 action-adventure game developed and published by Rockstar Games.',
-    }
-let gamePersona = {
-        name: 'Persona 5',//id = persona
-        price: 650000,
-        description: 'Persona 5 is a role-playing video game developed by Atlus. It is the sixth installment in the Persona series, which is part of the larger Megami Tensei franchise.',
-    }
 let gameTemp = ''
 let hargaTemp = 0
 let picTemp
